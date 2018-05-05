1dc16c61121a357727f9623a610d37e328048cbe
Andrea Tóth-Gayer <gayerandi@yahoo.com>
2018-05-05 13:03:58 +0200

    login logout

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport');
+var passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
 
 // Csatlakozás a mongoDB, auth nevű adatbázisához.
 const mongoose = require('mongoose');
@@ -18,13 +19,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(require('express-session')({ secret: 'little dad', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({
+  secret: 'little dad',
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,12 +39,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -46,5 +53,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// requires the model with Passport-Local Mongoose plugged in
+const User = require('./model/User');
+ 
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+ 
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,42 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../model/User");
+var passport = require('passport');
 
 // Beléptetjük a frissen regisztrált felhasználót.
 var authenticate = User.authenticate();
 var auth = (username, password, callBack) => {
-  authenticate(username, password, function(err, result) {
+  authenticate(username, password, function (err, result) {
     if (err) {
-      return callBack({ error: "User not athenticated." });
+      return callBack({
+        error: "User not athenticated."
+      });
     }
     // Sikeres belépés esetén visszaküldjük a felhasználó adatait.
     callBack(null);
   });
 };
 
-/* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.send("Hello Family!");
+});
+
+/* GET a user by ID. */
+router.get("/byusername/:username", function (req, res, next) {
+  User.findOne({
+      username: req.params.username
+    },
+    function (err, user) {
+      if (err) {
+        // ha nem találja
+        return res.json({
+          error: 'User not found'
+        });
+      } else {
+        res.json(user);
+      }
+    });
 });
 
 /**
@@ -24,34 +44,46 @@ router.get("/", function(req, res, next) {
  */
 router.post("/register", (req, res, next) => {
   // Registráljuk az új felhalsználót, a post body adatai alapján
-  User.register(
-    {
+  User.register({
       username: req.body.username,
       email: req.body.email,
       address: req.body.address
     },
     req.body.password,
-    function(err, newUser) {
+    function (err, newUser) {
       if (err) {
         // Regisztrációs hiba esetén.
-        return res.json({ error: "Bad registration data." });
+        return res.json({
+          error: "Bad registration data."
+        });
       }
 
       auth(req.body.username, req.body.password, (err) => {
-        if (err) { return res.json(err); }
+        if (err) {
+          return res.json(err);
+        }
         delete req.body.password;
         req.body._id = newUser._id;
         res.json(req.body);
-      });      
+      });
     }
   );
 });
 
 /**
- * Felhasznál beléptetése.
+ * Felhasználó beléptetése.
  */
-router.post('/login', (req, res, next) => {
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.redirect('/');
+});
 
+
+/**
+ * Felhasználó kiléptetése.
+ */
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/');
 });
 
 module.exports = router;
